7aedc0d0946ddaf6a734731bd1c626ca747faa96
Ryan Lima <contato@ryanlima.dev>
2023-06-18 19:43:39 -0300

    Solving TinyLogger::interpolate

--- src/TinyLogger.php
+++ src/TinyLogger.php
@@ -6,9 +6,8 @@ namespace TinyGears;
 
 use Stringable;
 use Psr\Log\{
-    LoggerInterface,
-    InvalidArgumentException,
-    LoggerTrait
+	LoggerInterface,
+	LoggerTrait
 };
 
 /**  
@@ -18,54 +17,52 @@ use Psr\Log\{
 
 final class TinyLogger implements LoggerInterface
 {
-    use LoggerTrait;
+	use LoggerTrait;
 
-    public function __construct(
-        private IStreamManager $streamManager,
-        private ?string $name = null
-    ) {
-    }
+	public function __construct(
+		private IStreamManager $streamManager,
+		private ?string $name = null
+	) {
+	}
 
-    /**
-     * Logs with an arbitrary level.
-     *
-     * @param mixed $level
-     * @param string|\Stringable $message
-     * @param array<mixed> $context
-     * @param array<string> $options
-     * 
-     * @return void
-     *
-     * @throws \Psr\Log\InvalidArgumentException
-     */
-    public function log($level, string|\Stringable $message, array $context = []): void
-    {
+	/**
+	 * Logs with an arbitrary level.
+	 *
+	 * @param mixed $level
+	 * @param string|\Stringable $message
+	 * @param array<mixed> $context
+	 * @param array<string> $options
+	 * 
+	 * @return void
+	 *
+	 * @throws \Psr\Log\InvalidArgumentException
+	 */
+	public function log($level, string|\Stringable $message, array $context = []): void
+	{
 
-        if ($this->streamManager instanceof IStreamManager) {
-            $this->streamManager->save($level, $message, $context, $this->name);
-        }
-    }
+		if ($this->streamManager instanceof IStreamManager) {
+			$this->streamManager->save($level, $message, $context, $this->name);
+		}
+	}
 
-    /**
-     * Used to replace the interpolations on the message with the value given in the $context
-     * @param string|\Stringable $message
-     * @param array<mixed> $context 
-     */
-    public static function interpolate(string|\Stringable $message, array $context = []): string
-    {
+	/**
+	 * Used to replace the interpolations on the message with the value given in the $context
+	 * @param string|\Stringable $message
+	 * @param array<mixed> $context 
+	 */
+	public static function interpolate(string|\Stringable $message, array $context = []): string
+	{
 
-        $replace = [];
-        array_walk(
-            $context,
-            function (mixed $value, string $key) use (&$replace): void {
-                if (!is_array($value) && (!is_object($value) || $value instanceof \Stringable)) {
-                    return;
-                }
-                $replace['{' . $key . '}'] = $value;
-            }
-        );
+		$replace = [];
+		array_walk(
+			$context,
+			function (mixed $value, string $key) use (&$replace): void {
+				if (is_string($value) || $value instanceof \Stringable) {
+					$replace['{' . $key . '}'] = $value;
+				}
+			}
+		);
 
-        return strtr((string) $message, $replace);
-    }
-
+		return strtr((string) $message, $replace);
+	}
 }
